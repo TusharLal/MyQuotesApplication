a03b53e930e8aaed89646df2bb5b9aa82c606579
tusharlal <tushar.lal@musictribe.com>
2025-08-18 18:37:38 +0530

    Initial commit of Android project

---
+++ app/src/main/java/com/example/myquotesapplication/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myquotesapplication.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.myquotesapplication.presentation.ui.theme.MyQuotesApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyQuotesApplicationTheme {
+
+            }
+        }
+    }
+}
